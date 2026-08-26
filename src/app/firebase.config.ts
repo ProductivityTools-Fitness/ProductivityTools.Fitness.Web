14058add6e57749c8bea90a1d6c23b7cce429ad0
Pawel Wujczyk <pwujczyk@gmail.com>
2026-08-26 21:59:44 +0200

    firebase

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAxmnZm4597RcMVQeJrby3HpuDfzYyTJoU",
+  authDomain: "ptprojectsweb.firebaseapp.com",
+  projectId: "ptprojectsweb",
+  storageBucket: "ptprojectsweb.firebasestorage.app",
+  messagingSenderId: "93484780890",
+  appId: "1:93484780890:web:3ab29c5382fb58f79ff763"
+};
