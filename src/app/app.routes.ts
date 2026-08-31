b08ddf90aa7e623e6883894084906ec3f5903499
Pawel Wujczyk <pwujczyk@gmail.com>
2026-08-31 21:23:41 +0000

    workout detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,21 +3,19 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () =>
-      import('./console/hello/hello.component').then((m) => m.HelloComponent),
+    loadComponent: () => import('./console/hello/hello.component').then((m) => m.HelloComponent),
   },
   {
-    path: 'workout',
+    path: 'workout-detail',
     loadComponent: () =>
       import('./workout/workout-detail/workout-detail.component').then(
         (m) => m.WorkoutDetailComponent,
       ),
   },
-    {
+  {
     path: 'exercise-list',
-    loadComponent: () =>
-      import('./exercise/list/list.component').then((m) => m.ListComponent),
+    loadComponent: () => import('./exercise/list/list.component').then((m) => m.ListComponent),
   },
+
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
-
--- src/app/workout/list/workout-list.ts
+++ src/app/workout/list/workout-list.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-workout-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './workout-list.html',
   styleUrl: './workout-list.css',
 })
