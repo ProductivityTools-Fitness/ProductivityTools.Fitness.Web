80b995a58431260e21dd8a8895d1219c5d048e49
Pawel Wujczyk <pwujczyk@gmail.com>
2026-09-02 20:09:21 +0000

    save traning

--- src/app/workout/workout-list/workout-list.component.ts
+++ src/app/workout/workout-list/workout-list.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component ,} from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { WorkoutService } from '../workout.service';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-workout-list',
@@ -7,5 +10,18 @@ import { RouterLink } from '@angular/router';
   templateUrl: './workout-list.component.html',
   styleUrl: './workout-list.component.css',
 })
-export class WorkoutListComponent {}
+export class WorkoutListComponent {
+  constructor(
+    private workoutService: WorkoutService) {}
 
+  newWorkout() {
+    this.workoutService.newWorkout().subscribe({
+      next: (entity) => {
+        
+        let workoutId=entity;
+        console.log('zapisano',workoutId)
+      },
+      error: (err) => console.log('blad', err),
+    });
+  }
+}
--- src/app/workout/workout.service.ts
+++ src/app/workout/workout.service.ts
@@ -1,4 +1,3 @@
-
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -8,17 +7,21 @@ import { environment } from '../../environments/environment';
   providedIn: 'root',
 })
 export class WorkoutService {
- private readonly http = inject(HttpClient);
+  private readonly http = inject(HttpClient);
 
-  updateExerciseList(workoutId:number, exerciseList:number[]): Observable<boolean> {
-    console.log("workoutid",workoutId);
-    console.log("exercise list",exerciseList);
-    return this.http.post<boolean>(`${environment.apiUrl}/workout/exerciseList`,{
-      workoutId:workoutId,
-      exerciseList:exerciseList
+  newWorkout() {
+    let workoutId:Observable<number> =this.http.post<number>(`${environment.apiUrl}/workout/newWorkout`,{
+      title:"xxx"
     });
+    return workoutId;
   }
 
+  updateExerciseList(workoutId: number, exerciseList: number[]): Observable<boolean> {
+    console.log('workoutid', workoutId);
+    console.log('exercise list', exerciseList);
+    return this.http.post<boolean>(`${environment.apiUrl}/workout/exerciseList`, {
+      workoutId: workoutId,
+      exerciseList: exerciseList,
+    });
+  }
 }
-
-
