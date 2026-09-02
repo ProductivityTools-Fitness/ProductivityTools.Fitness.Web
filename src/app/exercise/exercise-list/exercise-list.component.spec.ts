c5981423209f9292ffeb1322c02afbf9714f71b4
Pawel Wujczyk <pwujczyk@gmail.com>
2026-09-02 19:36:11 +0000

    improvements to save exercise

--- src/app/exercise/exercise-list/exercise-list.component.spec.ts
+++ src/app/exercise/exercise-list/exercise-list.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideRouter } from '@angular/router';
 
 import { ExerciseListComponent } from './exercise-list.component';
 
@@ -11,8 +12,13 @@ describe('ExerciseListComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ExerciseListComponent],
-      providers: [provideHttpClient(), provideHttpClientTesting()],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        provideRouter([]),
+      ],
     }).compileComponents();
+
 
 
     fixture = TestBed.createComponent(ExerciseListComponent);
--- src/app/exercise/exercise-list/exercise-list.component.ts
+++ src/app/exercise/exercise-list/exercise-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ExerciseService } from '../exercise.service';
 import { Exercise } from '../models/exercise';
 import { WorkoutService } from '../../workout/workout.service';
@@ -11,10 +12,13 @@ import { WorkoutService } from '../../workout/workout.service';
 })
 export class ExerciseListComponent implements OnInit {
   exerciseList = signal<Exercise[]>([]);
-  selectedExerciseIds=signal<Set<number>>(new Set());
+  selectedExerciseIds = signal<Set<number>>(new Set());
 
-  constructor(private exerciseService: ExerciseService,
-    private workoutService: WorkoutService
+  constructor(
+    private exerciseService: ExerciseService,
+    private workoutService: WorkoutService,
+    private route: ActivatedRoute,
+    private router: Router,
   ) {}
 
   ngOnInit() {
@@ -24,29 +28,35 @@ export class ExerciseListComponent implements OnInit {
     });
   }
 
-  toggleExercise(id:number){
-    this.selectedExerciseIds.update((current)=>{
-      const next=new Set(current);
-      if(next.has(id))
-      {
+  toggleExercise(id: number) {
+    this.selectedExerciseIds.update((current) => {
+      const next = new Set(current);
+      if (next.has(id)) {
         next.delete(id);
-      }
-      else
-      {
+      } else {
         next.add(id);
       }
       return next;
-  })
+    });
   }
 
-  saveExercises(){
-    let exerciseListArray= Array.from(this.selectedExerciseIds())
-    this.workoutService.updateExerciseList(1,exerciseListArray).subscribe({
-      next:()=>console.log("zapisano"),
-      error:(err)=>console.log("blad",err)
-    })
+  saveExercises() {
+    const workoutIdParam = this.route.snapshot.queryParamMap.get('workoutId');
+    const workoutId = workoutIdParam ? Number(workoutIdParam) : 1;
+    const exerciseListArray = Array.from(this.selectedExerciseIds());
+
+    this.workoutService.updateExerciseList(workoutId, exerciseListArray).subscribe({
+      next: () => {
+        console.log('Saved exercises for workout', workoutId);
+        this.router.navigate(['/workouts/detail'], {
+          queryParams: { workoutId },
+        });
+      },
+      error: (err) => console.error('Error saving exercises:', err),
+    });
   }
 }
+
 
 
 
